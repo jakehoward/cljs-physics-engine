3e84b3ed7a903c40182ffd75031603d243e764ab
jakehoward <jake.howard@tesglobal.com>
2017-08-08 21:30:48 +0100

    Add a simple animation to check physics engine
    - Co-ord system is back to front so things fall up

--- toy-app/project.clj
+++ toy-app/project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.854"
                   :exclusions [org.apache.ant/ant]]
-                 [quiescent "0.3.2"]]
+                 [quiescent "0.3.2"]
+                 [org.clojure/core.async "0.3.443"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.12"]
             [lein-doo "0.1.7"]]
--- toy-app/src/cljs/toy-app/core.cljs
+++ toy-app/src/cljs/toy-app/core.cljs
@@ -1,61 +1,46 @@
 (ns toy-app.core
   (:require [quiescent.core :as q]
             [quiescent.dom :as d]
-            [cljs-physics-engine.core :as physics]))
+            [cljs-physics-engine.core :as physics]
+            [clojure.core.async :refer [>! <! chan close! timeout]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-(defn- get-x-pos [nodes id]
-  (:x (first (filter #(= (:id %) id) nodes))))
-
-(defn- get-y-pos [nodes id]
-  (:y (first (filter #(= (:id %) id) nodes))))
-
-(defn- build-line-element [nodes] ;; Is this a legit pattern?
-  (fn [line]
-    (d/line {:x1 (get-x-pos nodes (:from line))
-             :y1 (get-y-pos nodes (:from line))
-             :x2 (get-x-pos nodes (:to line))
-             :y2 (get-y-pos nodes (:to line))
-             :key (str (:from line) (:to line))
-             :style {:stroke "rgb(59,59,59)" :strokeWidth "0.5"}})))
 
 (defn- build-circle-element [node]
   (d/circle {:cx (str (:x node))
              :cy (str (:y node))
-             :r "5"
+             :r "50"
              :key (:id node)
-             :stroke "green" :strokeWidth "0.25" :fill "yellow"}))
+             :stroke "yellow" :strokeWidth "5" :fill "blue"}))
 
-(defn- create-dom-elements [lines nodes]
-  (concat (map (build-line-element nodes) lines)
-          (map build-circle-element nodes)))
+(defn- create-dom-elements [nodes]
+  (map build-circle-element nodes))
 
-(defn add-positions [nodes]
-  (map #(merge %2 {:id (:id %1)}) nodes [{:x 75 :y 50} {:x 50 :y 75} {:x 25 :y 50} {:x 50 :y 25}]))
+(q/defcomponent Canvas [particles]
+  (d/div {:className "canvas"}
+         (d/svg {:width "1000" :height "600" :viewBox "0 0 1000 1000"}
+                (create-dom-elements particles))))
 
-(defn- lines-from-node [node]
-  (let [id (:id node)]
-    (map #(identity {:from id :to %}) (:links node))))
+(defn tick-every [ms]
+  (let [c (chan)]
+    (go-loop []
+      (<! (timeout ms))
+      (when (>! c :tick)
+        (recur)))
+    c))
 
-(defn- layout-graph [graph] ;; Better name?
-  (let [lines (mapcat lines-from-node (:nodes graph))
-        nodes (add-positions (:nodes graph))]
-    {:lines lines :nodes nodes}))
+(def t 0.05)
+(def env {:G 6.674e-11 :k-e 8.99e9 :size {:x 1000 :y 1000 :z 1000} :M 5.9722e24 :r 6.37e6})
+(def conns [])
 
-(q/defcomponent Canvas
-  "A home for our graph"
-  [graph]
-  (let [{:keys [lines nodes]} (layout-graph graph)]
-    (d/div {:className "canvas"}
-           (d/svg {:width "1000" :height "600" :viewBox "0 0 100 100"}
-                  (create-dom-elements lines nodes)))))
+(defn play-animation []
+  (go
+    (let [tick-chan (tick-every (* 1000 t))
+          particles [{:id 1 :m 10 :q 0 :x 500 :y 1000 :z 0 :v {:x 0 :y 0 :z 0}}]]
+      (loop [iteration 1 ps particles]
+        (when (and (<! tick-chan) (< iteration 200))
+          (q/render (Canvas ps) (.getElementById js/document "content"))
+          (recur (inc iteration) (:particles (physics/step-forward env conns t ps)))))
+      (close! tick-chan))))
 
-(def graph {:nodes [{:id 1 :links [2]}
-                    {:id 2 :links []}
-                    {:id 3 :links [2]}
-                    {:id 4 :links [1]}]})
-
-(println "Hey: " (physics/step-forward {:G 0 :k-e 0 :size {:x 1000 :y 1000 :z 1000} :M 100 :r 10} [] 2 [{:id 1 :m 0 :q 0 :x 0 :y 250 :z 0 :v {:x 0 :y -10 :z 0}}
-                     {:id 2 :m 10 :q 0 :x 0 :y 250 :z 0 :v {:x 0 :y -10 :z 0}}
-                     {:id 3 :m 5 :q 0 :x 0 :y 250 :z 0 :v {:x 0 :y -10 :z 0}}]))
-
-(q/render (Canvas graph) (.getElementById js/document "content"))
+(play-animation)
